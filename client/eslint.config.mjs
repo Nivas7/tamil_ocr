5f52b6ee21d5379eab52d0e9ec78fe84052a3ad9
Srinivas <nivasniyas24@gmail.com>
2025-05-10 14:41:39 +0530

    Chore: fixing Eslint and type's

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"], // Apply these rules to TypeScript files
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Turn off the `no-explicit-any` rule
+      "@typescript-eslint/no-empty-interface": "off", // Turn off the `typescript/no-empty-object` rule
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_", // Ignore unused arguments that start with `_`
+          varsIgnorePattern: "^_", // Ignore unused variables that start with `_`
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
