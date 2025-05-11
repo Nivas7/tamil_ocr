c62d65fe3e0db80394b497d45ee95dc8625ecf32
Srinivas <nivasniyas24@gmail.com>
2025-05-11 20:58:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,9 @@ def process_image():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello, tamil OCR!"})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
